84b66e860e6b32d60efd28850a6d20e5d6704ef9
anewholm <annesley_newholm@yahoo.it>
2023-06-10 10:20:15 +0100

    Better Warnings

--- models/_hint_dev_setup.php
+++ models/_hint_dev_setup.php
@@ -3,13 +3,16 @@ use DBAuth\PostGreSQLManager as DBManager;
 
 $username  = DBManager::configDatabase('username');
 $password  = DBManager::configDatabase('password');
-if ($username != '<DBAUTH>' || $password != '<DBAUTH>'):
+if ($username == 'winter'
+    && strstr($password, 'Quee') !== FALSE
+    && strstr($password, 'Poo')  !== FALSE
+):
 ?>
     <div class="layout-row min-size">
         <div class="callout callout-warning">
             <div class="header">
                 <i class="icon-warning"></i>
-                <h3><?= e(trans('Warning! Your .env contains a real username and password for the database. Please replace them with &quot;&lt;DBAUTH&gt;&quot; to enable this plugin.')) ?></h3>
+                <h3><?= e(trans('Warning! Your .env may contain the Development username and password for the database. Please replace them with &quot;&lt;DBAUTH&gt;&quot; to enable this plugin.')) ?></h3>
                 <p>
                     <?= e(trans('Edit your .env file directly to enable security.')) ?>
                 </p>
