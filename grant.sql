6995c4061e9945f1c834e932f86b1f9e05f55960
sanchez <sanchez@acornassociated.org>
2023-06-23 09:33:59 +0100

    Commit

--- grant.sql
+++ grant.sql
@@ -1,7 +1,7 @@
-CREATE USER "token_X" with CREATEROLE password 'password';
-GRANT ALL ON DATABaSE "winter" TO "token_X";
-GRANT ALL ON SCHEMA public TO "token_X";
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "token_X";
-GRANT ALL PRIVILEGES ON ALL sequences IN SCHEMA public TO "token_X";
-GRANT ALL PRIVILEGES ON ALL functions IN SCHEMA public TO "token_X";
-alter user "token_X" with createrole;
+CREATE USER "token_38" with CREATEROLE password 'password';
+GRANT ALL ON DATABaSE "winter" TO "token_38";
+GRANT ALL ON SCHEMA public TO "token_38";
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "token_38";
+GRANT ALL PRIVILEGES ON ALL sequences IN SCHEMA public TO "token_38";
+GRANT ALL PRIVILEGES ON ALL functions IN SCHEMA public TO "token_38";
+alter user "token_38" with createrole;
