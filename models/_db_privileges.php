c8f8715e1785b79ffbdaeb2ff9eebac112c39d85
sanchez <sanchez@acornassociated.org>
2025-02-19 17:00:15 +0300

    'no_message'

--- models/_db_privileges.php
+++ models/_db_privileges.php
@@ -1,42 +1,42 @@
-<div class="form-group checkbox-field" id="Form-field-User-acorn_grant_database_usage-group">
+<div class="form-group checkbox-field" id="Form-field-User-acornassociated_grant_database_usage-group">
     <div class="checkbox custom-checkbox" tabindex="0">
-        <input type="hidden" name="acorn_grant_database_usage" value="0">
-        <input type="checkbox" id="Form-field-User-acorn_grant_database_usage" name="acorn_grant_database_usage" value="1" checked="checked">
-        <label for="Form-field-User-acorn_grant_database_usage">Database usage privilege</label>
+        <input type="hidden" name="acornassociated_grant_database_usage" value="0">
+        <input type="checkbox" id="Form-field-User-acornassociated_grant_database_usage" name="acornassociated_grant_database_usage" value="1" checked="checked">
+        <label for="Form-field-User-acornassociated_grant_database_usage">Database usage privilege</label>
         <p class="help-block">Necessary for system usage.</p>
     </div>
 </div>
 
-<div class="form-group checkbox-field" id="Form-field-User-acorn_grant_schema_usage-group">
+<div class="form-group checkbox-field" id="Form-field-User-acornassociated_grant_schema_usage-group">
     <div class="checkbox custom-checkbox" tabindex="0">
-        <input type="hidden" name="acorn_grant_schema_usage" value="0">
-        <input type="checkbox" id="Form-field-User-acorn_grant_schema_usage" name="acorn_grant_schema_usage" value="1" checked="checked">
-        <label for="Form-field-User-acorn_grant_schema_usage">Schema usage privilege</label>
+        <input type="hidden" name="acornassociated_grant_schema_usage" value="0">
+        <input type="checkbox" id="Form-field-User-acornassociated_grant_schema_usage" name="acornassociated_grant_schema_usage" value="1" checked="checked">
+        <label for="Form-field-User-acornassociated_grant_schema_usage">Schema usage privilege</label>
         <p class="help-block">Necessary for system usage.</p>
     </div>
 </div>
 
-<div class="form-group checkbox-field" id="Form-field-User-acorn_grant_tables_all-group">
+<div class="form-group checkbox-field" id="Form-field-User-acornassociated_grant_tables_all-group">
     <div class="checkbox custom-checkbox" tabindex="0">
-        <input type="hidden" name="acorn_grant_tables_all" value="0">
-        <input type="checkbox" id="Form-field-User-acorn_grant_tables_all" name="acorn_grant_tables_all" value="1" checked="checked">
-        <label for="Form-field-User-acorn_grant_tables_all">All Tables privileges</label>
+        <input type="hidden" name="acornassociated_grant_tables_all" value="0">
+        <input type="checkbox" id="Form-field-User-acornassociated_grant_tables_all" name="acornassociated_grant_tables_all" value="1" checked="checked">
+        <label for="Form-field-User-acornassociated_grant_tables_all">All Tables privileges</label>
     </div>
 </div>
 
-<div class="form-group checkbox-field" id="Form-field-User-acorn_grant_sequences_all-group">
+<div class="form-group checkbox-field" id="Form-field-User-acornassociated_grant_sequences_all-group">
     <div class="checkbox custom-checkbox" tabindex="0">
-        <input type="hidden" name="acorn_grant_sequences_all" value="0">
-        <input type="checkbox" id="Form-field-User-acorn_grant_sequences_all" name="acorn_grant_sequences_all" value="1" checked="checked">
-        <label for="Form-field-User-acorn_grant_sequences_all">All Sequences privileges</label>
+        <input type="hidden" name="acornassociated_grant_sequences_all" value="0">
+        <input type="checkbox" id="Form-field-User-acornassociated_grant_sequences_all" name="acornassociated_grant_sequences_all" value="1" checked="checked">
+        <label for="Form-field-User-acornassociated_grant_sequences_all">All Sequences privileges</label>
     </div>
 </div>
 
-<div class="form-group checkbox-field" id="Form-field-User-acorn_grant_functions_all-group">
+<div class="form-group checkbox-field" id="Form-field-User-acornassociated_grant_functions_all-group">
     <div class="checkbox custom-checkbox" tabindex="0">
-        <input type="hidden" name="acorn_grant_functions_all" value="0">
-        <input type="checkbox" id="Form-field-User-acorn_grant_functions_all" name="acorn_grant_functions_all" value="1" checked="checked">
-        <label for="Form-field-User-acorn_grant_functions_all">All Functions privileges</label>
+        <input type="hidden" name="acornassociated_grant_functions_all" value="0">
+        <input type="checkbox" id="Form-field-User-acornassociated_grant_functions_all" name="acornassociated_grant_functions_all" value="1" checked="checked">
+        <label for="Form-field-User-acornassociated_grant_functions_all">All Functions privileges</label>
     </div>
 </div>
 
